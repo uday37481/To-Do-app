5135cdc88bb80bf9b3b6df4bf37de7e5cfc9a0b9
Uday Nagar <uvnagar370823@kkwagh.edu.in>
2025-09-07 14:30:49 +0530

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -162,3 +162,20 @@ list.addEventListener('dragover', (e) => {
 		list.insertBefore(dragEl, after);
 	}
 });
+
+function getDragAfterElement(container, y) {
+	const els = [...container.querySelectorAll('.item:not(.dragging)')];
+	return els.reduce((closest, child) => {
+		const box = child.getBoundingClientRect();
+		const offset = y - box.top - box.height / 2;
+		if (offset < 0 && offset > closest.offset) {
+			return { offset, element: child };
+		} else {
+			return closest;
+		}
+	}, { offset: Number.NEGATIVE_INFINITY }).element;
+}
+
+// Initial paint
+computeProgress();
+setEmptyState();
