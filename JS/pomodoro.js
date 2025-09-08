ab344d8f1cd13b5ae78f7270f7ded4930e375e30
Uday Nagar <uvnagar370823@kkwagh.edu.in>
2025-09-08 22:22:19 +0530

    Create pomodoro.js

---
+++ JS/pomodoro.js
@@ -0,0 +1,10 @@
+// Feature 4: Pomodoro Timer
+// Timer for focused work sessions
+
+export function startPomodoro(durationMinutes = 25) {
+  // Start a pomodoro timer
+}
+
+export function stopPomodoro() {
+  // Stop the timer
+}
