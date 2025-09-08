3f34b7c658974960a9d21d14e6f3c686e79515c4
Uday Nagar <uvnagar370823@kkwagh.edu.in>
2025-09-08 22:21:32 +0530

    Create voice-input.js

---
+++ JS/voice-input.js
@@ -0,0 +1,10 @@
+// Feature: Voice Input
+// Add tasks using voice commands
+
+export function initVoiceInput() {
+  // Initialize speech recognition
+}
+
+export function listenForTask() {
+  // Start listening for a new task
+}
