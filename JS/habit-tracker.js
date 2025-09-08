7faa4e433279a68efb6a281d1b65bda7e635d0d9
Uday Nagar <uvnagar370823@kkwagh.edu.in>
2025-09-08 23:04:06 +0530

    Create habit-tracker.js

---
+++ JS/habit-tracker.js
@@ -0,0 +1,10 @@
+// Feature: Habit Tracker
+// Track recurring tasks and visualize streaks
+
+export function trackHabit(taskId) {
+  // Increment streak for the given task
+}
+
+export function getHabitStreak(taskId) {
+  // Return current streak for the given task
+}
